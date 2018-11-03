c3f5389d6ce9f0690eb2dc2e5bab292fe47fd1ab
chinatsukawakami <zarusoba3939@gmail.com>
2018-11-03 00:06:28 -0400

    0.0.2 add bmi.js,home.html,and style.css

---
+++ bmi.js
@@ -0,0 +1,24 @@
+/*
+ * Author : Chinatsu Kawakami
+ * Created Date: 2nd Nov 2018
+ * Name: bmi.js
+ * Version 0.0.1
+ * Description: This is BMI Calculator
+ * */
+
+//Fields 
+
+
+var height;
+var weight;
+var inch;
+var pounds;
+var btn; 
+var weightbox;
+var heightbox;
+var resultbox;
+var result;
+
+var standard;
+var metric;
+
